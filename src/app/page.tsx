093c64693073b5063e9fe2e69dcb54de6ff2b1c4
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2025-07-20 12:48:19 +0530

    feat: add theme toggle, fix biome config✨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
+import { ThemeToggle } from '@/components/theme-toggle';
 
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+      <ThemeToggle />
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
         <Image
           alt="Next.js logo"
           className="dark:invert"
           height={38}
           priority
-          src='/next.svg'
+          src="/next.svg"
           width={180}
         />
         <ol className="list-inside list-decimal text-center font-mono text-sm/6 sm:text-left">
